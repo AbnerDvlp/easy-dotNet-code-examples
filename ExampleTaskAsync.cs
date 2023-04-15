7e470faf5a42b026d1f93f824eaf5277e1b0994a
AbnerDvlp <abnerantonio_marcelino@yahoo.com.br>
2023-04-15 09:45:00 -0300

    include async task time test

---
+++ ExampleTaskAsync.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace DotNetCodeExamples
+{
+    internal class ExampleTaskAsync
+    {
+        // classe com exemplo da diferença entre execução sincorna e assincrona retornando seus tempos de execução
+        // class with example of the difference between synchronous and asynchronous execution returning their execution times
+
+        public async Task ExecuteValidationAsync()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("Running validation one");
+        }
+
+        public async Task ExecuteSequentialMode()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            await ExecuteValidationAsync();
+            await ExecuteValidationAsync();
+            stopwatch.Stop();
+            Console.WriteLine("Execution time of the method in sequential mode was {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        public async Task ExecuteParallelMode()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            var tarefasValidacao = new List<Task>();
+            stopwatch.Start();
+            tarefasValidacao.Add(ExecuteValidationAsync());
+            tarefasValidacao.Add(ExecuteValidationAsync());
+            await Task.WhenAll(tarefasValidacao);
+            stopwatch.Stop();
+            Console.WriteLine("Execution time of the method in parallel mode was: {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        public async Task TestMethodsAsync()
+        {
+            await ExecuteParallelMode();
+            await ExecuteSequentialMode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using DotNetCodeExamples;
+
+/*Comente a instacia da classe que não for usar e chame o metodo de teste da classe ou qualquer outro que queira testar*/
+/*Comment out the instance of the class you won 't be using and call the test method of the class or any other method you want to test.*/
+
+ExampleTaskAsync operacoes = new ExampleTaskAsync();
+
+await operacoes.TestMethodsAsync();
